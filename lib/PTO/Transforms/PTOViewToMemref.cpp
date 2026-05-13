0b94f8340a8769f151f6d520682394133bece1cf
zhangstevenunity <128771452+zhangstevenunity@users.noreply.github.com>
2026-05-13 09:50:07 +0800

    fix: preserve tmatmul acc phase lowering

--- lib/PTO/Transforms/PTOViewToMemref.cpp
+++ lib/PTO/Transforms/PTOViewToMemref.cpp
@@ -1702,7 +1702,8 @@ struct PTOViewToMemrefPass
         Value rhs = op->getOperand(1);
         Value dst = op->getOperand(2);
 
-        rewriter.replaceOpWithNewOp<pto::TMatmulOp>(op, TypeRange{}, lhs, rhs, dst);
+        rewriter.replaceOpWithNewOp<pto::TMatmulOp>(
+            op, TypeRange{}, lhs, rhs, dst, op.getAccPhaseAttr());
       }
 
       // --- TMatmulAccOp [Acc, Lhs, Rhs, Dst] ---
@@ -1713,7 +1714,8 @@ struct PTOViewToMemrefPass
         rewriter.setInsertionPoint(op);
         rewriter.replaceOpWithNewOp<pto::TMatmulAccOp>(
           op, TypeRange{}, 
-          op->getOperand(0), op->getOperand(1), op->getOperand(2), op->getOperand(3));
+          op->getOperand(0), op->getOperand(1), op->getOperand(2),
+          op->getOperand(3), op.getAccPhaseAttr());
       }
 
       // --- TMatmulBiasOp [Acc, Lhs, Rhs, Bias, Dst] ---
